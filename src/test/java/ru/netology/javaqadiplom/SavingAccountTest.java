77b131f70fd3d9b17f1318395f5e496bb8d0608e
Andrey Kononenko <anaruw_13@mail.ru>
2025-02-06 19:24:10 +0300

    Update SavingAccountTest.java

--- src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/SavingAccountTest.java
@@ -4,14 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SavingAccountTest {
 
-    /**
-     * Под каждым параметризованным тестом оставил тест-Template, чтобы можно было прогнать проверку по конкретным параметрам,
-     * а не весь тест целиком. Перед финальной проверкой убедиться, что набор данных в этих тестах не вызовет ошибку,
-     * или просто закомментировать.
-     */
     int minBalance = 1_000;                     //
     int initialBalance = minBalance + 1_000;    // изменяя эти поля можно нарушить актуальность
     int maxBalance = minBalance + 9_000;        //          параметризованных тестов
@@ -30,7 +26,6 @@ public class SavingAccountTest {
 
         Assertions.assertEquals(2_000 + 3_000, account.getBalance());
     }
-    //-------------------------------------------------------------------------------------------\\
 
     @ParameterizedTest
     @CsvSource(value = {
@@ -46,22 +41,6 @@ public class SavingAccountTest {
         Assertions.assertEquals(rate, account.getRate());
         Assertions.assertEquals(minBalance, account.getMinBalance());
         Assertions.assertEquals(maxBalance, account.getMaxBalance());
-    }
-
-    @Test
-    public void createSavingAccountTestTemplate() {
-
-        initialBalance = 0;
-        minBalance = 0;
-        maxBalance = 0;
-        rate = 0;
-
-        SavingAccount account = new SavingAccount(initialBalance, minBalance, maxBalance, rate);
-
-        Assertions.assertEquals(initialBalance, account.getBalance());
-        Assertions.assertEquals(minBalance, account.getMinBalance());
-        Assertions.assertEquals(maxBalance, account.getMaxBalance());
-        Assertions.assertEquals(rate, account.getRate());
     }
 
     //-----ожидаемое сообщение в следующих тестах можно подкорректировать, главное- чтобы улавливался смысл исключения-----------\\
@@ -157,78 +136,52 @@ public class SavingAccountTest {
     //----------------------------------------------------------------------------------------------------------------------------\\
 
     @ParameterizedTest
-    @CsvSource(value = {
-            "-1,false,2000",
-            "0,false,2000",
-            "1,true,1999",
-            "999,true,1001",
-            "1000,true,1000",
-            "1001,false,2000"
-    })
-    public void paymentTest(int amount, boolean expectedResult, int expectedBalance) {
+    @ValueSource(ints = {-1, 0, 1, 999, 1000, 1001})
+    public void paymentTest(int amount) {
 
         SavingAccount account = new SavingAccount(initialBalance, minBalance, maxBalance, rate);
 
-        boolean actualResult = account.pay(amount);
-        Assertions.assertEquals(expectedResult, actualResult);
+        boolean expectedResult;
+        int expectedBalance;
 
+        if ((initialBalance - amount) < minBalance || amount <= 0) {
+
+            expectedResult = false;
+            expectedBalance = initialBalance;
+        } else {
+            expectedResult = true;
+            expectedBalance = initialBalance - amount;
+        }
+        boolean actualResult = account.pay(amount);
         int actualBalance = account.getBalance();
+
+        Assertions.assertEquals(expectedResult, actualResult);
         Assertions.assertEquals(expectedBalance, actualBalance);
     }
-
-    @Test
-    public void paymentTestTemplate() {
-
-        int amount = 1;
-        boolean expectedResult = true;
-        int expectedBalance = 1999;
-
-        SavingAccount account = new SavingAccount(initialBalance, minBalance, maxBalance, rate);
-
-        boolean actualResult = account.pay(amount);
-        Assertions.assertEquals(expectedResult, actualResult);
-
-        int actualBalance = account.getBalance();
-        Assertions.assertEquals(expectedBalance, actualBalance);
-    }
-    //----------------------------------------------------------------------------------------------------\\
 
     @ParameterizedTest
-    @CsvSource(value = {
-            "-1,false,2000",
-            "0,false,2000",
-            "1,true,2001",
-            "7999,true,9999",
-            "8000,true,10000",
-            "8001,false,2000"
-    })
-    public void addTest(int amount, boolean expectedResult, int expectedBalance) {
+    @ValueSource(ints = {-1, 0, 1, 7999, 8000, 8001})
+    public void addTest(int amount) {
 
         SavingAccount account = new SavingAccount(initialBalance, minBalance, maxBalance, rate);
 
-        boolean actualResult = account.add(amount);
-        Assertions.assertEquals(expectedResult, actualResult);
+        boolean expectedResult;
+        int expectedBalance;
 
+        if ((initialBalance + amount) > maxBalance || amount <= 0) {
+
+            expectedResult = false;
+            expectedBalance = initialBalance;
+        } else {
+            expectedResult = true;
+            expectedBalance = initialBalance + amount;
+        }
+        boolean actualResult = account.add(amount);
         int actualBalance = account.getBalance();
+
+        Assertions.assertEquals(expectedResult, actualResult);
         Assertions.assertEquals(expectedBalance, actualBalance);
     }
-
-    @Test
-    public void addTestTemplate() {
-
-        int amount = 1;
-        boolean expectedResult = true;
-        int expectedBalance = 2001;
-
-        SavingAccount account = new SavingAccount(initialBalance, minBalance, maxBalance, rate);
-
-        boolean actualResult = account.add(amount);
-        Assertions.assertEquals(expectedResult, actualResult);
-
-        int actualBalance = account.getBalance();
-        Assertions.assertEquals(expectedBalance, actualBalance);
-    }
-    //---------------------------------------------------------------------------------------------\\
 
     @Test
     public void yearChange() {
