0cd11b763d061f05c0b15edbc198a2dc76a32d40
Andrey Kononenko <57371408+anaruw@users.noreply.github.com>
2025-02-08 22:37:43 +0300

    Revert "Fix issue #1, #2, #4, #6"

--- src/main/java/ru/netology/javaqadiplom/SavingAccount.java
+++ src/main/java/ru/netology/javaqadiplom/SavingAccount.java
@@ -14,39 +14,17 @@ public class SavingAccount extends Account {
      * Создаёт новый объект сберегательного счёта с заданными параметрами.
      * Если параметры некорректны (мин. баланс больше максимального и так далее), то
      * должно выкидываться исключения вида IllegalArgumentException.
-     *
      * @param initialBalance - начальный баланс
-     * @param minBalance     - минимальный баланс
-     * @param maxBalance     - максимальный баланс
-     * @param rate           - неотрицательное число, ставка в процентах годовых на остаток
+     * @param minBalance - минимальный баланс
+     * @param maxBalance - максимальный баланс
+     * @param rate - неотрицательное число, ставка в процентах годовых на остаток
      */
     public SavingAccount(int initialBalance, int minBalance, int maxBalance, int rate) {
         if (rate < 0) {
             throw new IllegalArgumentException(
-                    "Накопительная ставка не может быть отрицательной, а у вас: " + rate
+              "Накопительная ставка не может быть отрицательной, а у вас: " + rate
             );
         }
-        if (minBalance < 0) {
-            throw new IllegalArgumentException(
-                    "Минимальный баланс не может быть отрицательным, а у вас: " + minBalance
-            );
-        }
-        if (maxBalance < minBalance) {
-            throw new IllegalArgumentException(
-                    "Максимальный баланс не может быть меньше минимального, а у вас: " + maxBalance
-            );
-        }
-        if (initialBalance < minBalance) {
-            throw new IllegalArgumentException(
-                    "Начальный баланс не может быть меньше минимального, а у вас: " + initialBalance
-            );
-        }
-        if (initialBalance > maxBalance) {
-            throw new IllegalArgumentException(
-                    "Начальный баланс не может быть больше максимального, а у вас: " + initialBalance
-            );
-        }
-
         this.balance = initialBalance;
         this.minBalance = minBalance;
         this.maxBalance = maxBalance;
@@ -59,7 +37,6 @@ public class SavingAccount extends Account {
      * на сумму покупки. Если же операция может привести к некорректному
      * состоянию счёта (например, баланс может уйти в минус), то операция должна
      * завершиться вернув false и ничего не поменяв на счёте.
-     *
      * @param amount - сумма покупки
      * @return true если операция прошла успешно, false иначе.
      */
@@ -68,8 +45,8 @@ public class SavingAccount extends Account {
         if (amount <= 0) {
             return false;
         }
-        if (balance - amount >= minBalance) {
-            balance -= amount;
+        balance = balance - amount;
+        if (balance > minBalance) {
             return true;
         } else {
             return false;
@@ -82,10 +59,9 @@ public class SavingAccount extends Account {
      * на сумму покупки. Если же операция может привести к некорректному
      * состоянию счёта, то операция должна
      * завершиться вернув false и ничего не поменяв на счёте.
-     *
      * @param amount - сумма пополнения
-     * @param amount
      * @return true если операция прошла успешно, false иначе.
+     * @param amount
      * @return
      */
     @Override
@@ -93,8 +69,8 @@ public class SavingAccount extends Account {
         if (amount <= 0) {
             return false;
         }
-        if (balance + amount <= maxBalance) {
-            balance += amount;
+        if (balance + amount < maxBalance) {
+            balance = amount;
             return true;
         } else {
             return false;
@@ -106,12 +82,11 @@ public class SavingAccount extends Account {
      * счёт не будет меняться год. Сумма процентов приводится к целому
      * числу через отбрасывание дробной части (так и работает целочисленное деление).
      * Пример: если на счёте 200 рублей, то при ставке 15% ответ должен быть 30.
-     *
      * @return
      */
     @Override
     public int yearChange() {
-        return (int)((double)balance / 100 * rate);
+        return balance / 100 * rate;
     }
 
     public int getMinBalance() {
